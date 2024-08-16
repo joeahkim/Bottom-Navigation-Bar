6d95d8af4d81292b3618e40cbfc62eda1fa4e1da
joakim ireri <joeahkim@gmail.com>
2024-08-16 10:46:28 +0300

    first commit

---
+++ app/src/main/java/com/joeahkim/bottomnavigationbar/pages/NotificationPage.kt
@@ -0,0 +1,32 @@
+package com.joeahkim.bottomnavigationbar.pages
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun NotificationPage(modifier: Modifier = Modifier) {
+    Column (
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.LightGray),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(
+            text = "Notification Page",
+            fontSize = 40.sp,
+            color = Color.White,
+            fontWeight = FontWeight.Bold
+        )
+
+    }
+}
